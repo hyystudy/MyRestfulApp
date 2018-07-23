049291b0bdc5cbcb8cf07e421e3a77a7eab5f047
贺洋洋 <768061025@qq.com>
2018-07-23 15:08:17 +0800

    done

---
+++ app/src/main/java/com/example/administrator/myrestfulapp/ViewPagerActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myrestfulapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/7/23.
+ */
+
+public class ViewPagerActivity  extends AppCompatActivity{
+}
